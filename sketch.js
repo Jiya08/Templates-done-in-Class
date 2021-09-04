fbe4df9a553b302d93b6ea2259bce1b247aa4e40
Jiya08 <88022024+Jiya08@users.noreply.github.com>
2021-09-04 11:35:47 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,12 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-
-function preload() {
-    trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-    trex_collided = loadImage("trex_collided.png");
-    groundImage = loadImage("ground2.png")
-}
+var student;
 
 function setup() {
-    createCanvas(600, 200);
+  createCanvas(400,400);
+student = new Student("Joseph",11,"A");
+}
 
-    //create a trex sprite
-    trex = createSprite(50,160,20,50);
-    trex.addAnimation("running", trex_running);
-    trex.scale = 0.5;
-
-    //create a ground sprite
-    ground = createSprite(200,180,400,20);
-    ground.addImage("ground",groundImage);
-    ground.x = ground.width /2;
-    ground.velocityX = -4;
-
-    //create an invisible ground
-    invisibleGround = createSprite(300,190,600,10);  
-    invisibleGround.visible = false;
-}   
-  
-function draw() {
-    background(220);
-
-    //jump when the space button is pressed
-    if (keyDown("space") && trex.y>= 150) {
-    trex.velocityY = -10;
-    }
-
-    trex.velocityY = trex.velocityY + 0.8
-    if (ground.x < 0) {
-    ground.x = ground.width / 2;
-    }
-
-    trex.collide(invisibleGround);
-    drawSprites();
-
-    console.log(trex.y);
+function draw() 
+{
+  background(30);
+  student.show();
 }
